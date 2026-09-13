e6e518687714075b74c8e8dd2f01523dca5e5042
ampere <xxup@me.com>
2026-09-14 01:55:11 +0800

    更新 script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
   var P5_DIR = "p5/";
   var P5_CDN =
     "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js";
-  var P5_FILES = ["sketch1.js", "sketch2.js", "sketch3.js"];
+  var P5_FILES = ["dots.js", "orbit.js", "rotating-square.js"];
 
   /* AI助手配置 */
   var AI_KEY_STORAGE = "p5_ai_keys";
